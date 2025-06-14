65104ef8706c00c62f8b3d15389798c9abf18d28
Maroua <marouamekimene@gmail.com>
2025-06-14 23:47:26 +0100

    Add controller to return a single post by ID

--- backend/01-intro-to-express/starter/src/controllers/postsController.js
+++ backend/01-intro-to-express/starter/src/controllers/postsController.js
@@ -3,3 +3,12 @@ import posts from "../data/posts.js";
 export const getPosts = (req, res) => {
   res.json(posts);
 };
+
+export const getPostById = (req, res) => {
+  const Id = parseInt(req.params.id);
+  const post = posts.find((p) => p.id === Id);
+  if (!post) {
+    return res.status(404).json({ error: "Post not found" });
+  } 
+  res.json(post);
+  }
