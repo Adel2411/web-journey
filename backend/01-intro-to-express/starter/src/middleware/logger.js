8a2fc2194f15df2fb0f56eb14b319916f364c852
Maroua <marouamekimene@gmail.com>
2025-06-15 18:48:29 +0100

    Added stringify to header and body to be able to display them

--- backend/01-intro-to-express/starter/src/middleware/logger.js
+++ backend/01-intro-to-express/starter/src/middleware/logger.js
@@ -1,19 +1,10 @@
-export default function logger(req, res, next) { //
+export default function logger(req, res, next) {
   const timestamp = new Date().toISOString();
   const method = req.method;
   const url = req.originalUrl;
-  const headers = req.headers;
-  let bodyContent;
-  if (req.body) {
-    bodyContent = console.log(req.body);
-  } 
-  else { 
-    bodyContent = (console.log("undefined"));
-  }
-
-  const body = bodyContent;
+  const headers = JSON.stringify(req.headers);
+  const body = req.body && Object.keys(req.body).length > 0? JSON.stringify(req.body): "undefined";
 
   console.log(`[${timestamp}] ${method} ${url} - Headers: ${headers} - Body: ${body}`);
-  
   next();
 }
