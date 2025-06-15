b7f289b365e8f432f9982da65ebbf3c8303bc42b
amiraber <155098066+amiraber@users.noreply.github.com>
2025-06-15 12:16:58 +0200

    🎯 Completed backend API challenge

--- backend/01-intro-to-express/starter/src/routes/posts.js
+++ backend/01-intro-to-express/starter/src/routes/posts.js
@@ -1,7 +1,28 @@
 import express from "express";
 import logger from "../middleware/logger.js";
-import { getPosts } from "../controllers/postsController.js";
+import { addTimestamps, updateTimestamp } from "../middleware/timesatamp.js";
+import { getPosts, getPostById, getPostsByAuthor, createPost, updatePost, deletePost } from "../controllers/postsController.js";
+import { validatePost, validatePut } from "../middleware/validation.js";
 
 export const routes = express.Router();
 
-routes.get("/", logger, getPosts);
+// GET /posts or /posts?author=name
+routes.get("/", logger, (req, res, next) => {
+    if(req.query.author) {
+        return getPostsByAuthor(req, res);
+    } else {
+        return getPosts(req, res);
+    }
+});
+
+// GET /posts/:id
+routes.get("/:id", getPostById);
+
+// POST /posts to add a new blog post
+routes.post("/", logger, validatePost, addTimestamps, createPost);
+
+// PUT /posts/:id
+routes.put("/:id", logger, validatePut, updateTimestamp, updatePost);
+
+// DELETE /posts/:id
+routes.delete("/:id", logger, deletePost);
