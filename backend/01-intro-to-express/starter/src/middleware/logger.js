9d673c5cd1e29b1e2ac73f415fd2b0c5aa90b0e7
Maroua <marouamekimene@gmail.com>
2025-06-15 17:35:23 +0100

    Add logger middleware

--- backend/01-intro-to-express/starter/src/middleware/logger.js
+++ backend/01-intro-to-express/starter/src/middleware/logger.js
@@ -1,4 +1,19 @@
-export default function logger(req, res, next) {
-  console.log("Request Headers:", req.headers);
+export default function logger(req, res, next) { //
+  const timestamp = new Date().toISOString();
+  const method = req.method;
+  const url = req.originalUrl;
+  const headers = req.headers;
+  let bodyContent;
+  if (req.body) {
+    bodyContent = console.log(req.body);
+  } 
+  else { 
+    bodyContent = (console.log("undefined"));
+  }
+
+  const body = bodyContent;
+
+  console.log(`[${timestamp}] ${method} ${url} - Headers: ${headers} - Body: ${body}`);
+  
   next();
 }
