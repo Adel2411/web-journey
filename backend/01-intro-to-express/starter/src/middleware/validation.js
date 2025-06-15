c8f2c3e5c144ead4f9fd7ecb26b02e5cba1084e2
Maroua <marouamekimene@gmail.com>
2025-06-15 18:00:43 +0100

    Add PUT  validation middleware to ensure at least one non-empty string is provided for update

--- backend/01-intro-to-express/starter/src/middleware/validation.js
+++ backend/01-intro-to-express/starter/src/middleware/validation.js
@@ -2,13 +2,13 @@ export function validationPostCreateBlogPost (req, res, next) {
     const post =req.body;
     const wrongFileds = [];
 
-    if (!title || typeof title !== "string" || post.title.trim() === "" ) 
+    if (!post.title || typeof post.title !== "string" || post.title.trim() === "" ) 
         wrongFileds.push("title");
 
-    if (!content || typeof content !== "string" || post.content.trim() === "")
+    if (!post.content || typeof post.content !== "string" || post.content.trim() === "")
         wrongFileds.push("content");
 
-    if (!author || typeof author !== "string" || post.author.trim() === "")
+    if (!post.autho || typeof post.autho !== "string" || post.author.trim() === "")
         wrongFileds.push("author");
 
     if (wrongFileds.length > 0) {
@@ -16,6 +16,23 @@ export function validationPostCreateBlogPost (req, res, next) {
             error: "Validation failed",
             message: "Missing required fields:" + wrongFileds.join(", "),
         });
+    }
+    next();
+}
+
+export function validationPutUpdateBlogPostById (req, res, next) {
+    const post = req.body;
+
+      // Check if none of the fields are provided
+    if (!post.title  && !post.content && !post.author) {
+      return res.status(400).json({ error: "Validation failed", message: "At least one field (title, content, or author) must be provided",});
+    }
+
+    if (
+       (post.title || typeof post.title !== "string" || post.title.trim() === "") ||
+       (post.content || typeof post.content !== "string" || post.content.trim() === "") ||
+       (post.autho || typeof post.autho !== "string" || post.autho.trim() === "")) { 
+       return res.status(400).json({ error: "Validation failed", message: "All provided fields must be non-empty strings",});
     }
     next();
 }
