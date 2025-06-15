651624589c82d70079cfc1dfe7196e6134d2e6db
Maroua Ziouche <maroua.ziouche@etu.usthb.dz>
2025-06-15 10:58:40 +0100

    Remove repeating logger middleware and apply it globally in the app.js file

--- backend/01-intro-to-express/starter/src/routes/posts.js
+++ backend/01-intro-to-express/starter/src/routes/posts.js
@@ -1,14 +1,11 @@
 import express from "express";
-import logger from "../middleware/logger.js";
 import timestamp from "../middleware/timesatamp.js";
 import validation from "../middleware/validation.js";
 import { handleGetPosts ,getPostById  ,createPost,updatePostById,deletePostById } from "../controllers/postsController.js";
 
 export const routes = express.Router();
-routes.get("/:id",logger,getPostById);
-routes.get("/", logger, handleGetPosts);
-
-
-routes.post("/",logger,timestamp,validation,createPost);
-routes.put("/:id",logger,timestamp,validation,updatePostById);
-routes.delete("/:id",logger,deletePostById);
+routes.get("/:id",getPostById);
+routes.get("/", handleGetPosts);
+routes.post("/",timestamp,validation,createPost);
+routes.put("/:id",timestamp,validation,updatePostById);
+routes.delete("/:id",deletePostById);
