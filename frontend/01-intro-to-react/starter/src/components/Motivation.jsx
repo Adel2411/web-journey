8037d9f0c4cee0b09e0eb75c6b6fc180c51c823a
bouhamza abderrahmane <bouhamzaabouda@gmail.com>
2025-06-18 09:17:22 +0100

    finished working on like counter

--- frontend/01-intro-to-react/starter/src/components/Motivation.jsx
+++ frontend/01-intro-to-react/starter/src/components/Motivation.jsx
@@ -1,7 +1,31 @@
 import "../styles/Motivation.css";
+import { useState } from "react";
 
 function Motivation() {
-  return <div className="motivation">Motivation</div>;
+  const [likes, setLikes] = useState(0);
+  const [animate, setAnimate] = useState(false);
+
+  const likesCounter = () => {
+  setLikes((likes) => {
+    return likes + 1;
+  });
+   setAnimate(true);
+    setTimeout(() => setAnimate(false), 500);
+}
+   
+  return (
+    <div className="motivation">
+      <h3>❤️ Motivation</h3>
+      <button
+        onClick={likesCounter}
+        className={`like-btn ${animate ? "pulse" : ""}`}
+        
+      >
+        Like
+      </button>
+      <p>{likes} {likes === 1 ? "like" : "likes"}</p>
+    </div>
+  );
 }
 
 export default Motivation;
