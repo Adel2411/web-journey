ef6ab3e238a9a8c7d707eac47bc07766e57dc69a
yasminebounasla <162790511+yasminebounasla@users.noreply.github.com>
2025-06-18 21:39:40 +0200

    Update posts.js

--- backend/01-intro-to-express/starter/src/routes/posts.js
+++ backend/01-intro-to-express/starter/src/routes/posts.js
@@ -1,8 +1,16 @@
 import express, { Router } from "express";
 import logger from "../middleware/logger.js";
-import { getPosts } from "../controllers/postsController.js";
+import { postValidator, putValidator } from "../middleware/validation.js";
+import { creatPost, deletPost, getPostByAuthor, getPostById, getPosts, updatPost } from "../controllers/postsController.js";
 
 export const routes = express.Router();
 
+
+routes.get("/author", getPostByAuthor);
+routes.get("/:id", getPostById);
 routes.get("/", logger, getPosts);
+routes.get("/", getPosts);
+routes.post("/",postValidator, creatPost);
+routes.put("/:id",putValidator, updatPost);
+routes.delete("/:id", deletPost);
 
