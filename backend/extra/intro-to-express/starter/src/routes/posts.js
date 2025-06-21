03b3276f56732cb2f48b2b32f56991623a18fd5f
amiraber <155098066+amiraber@users.noreply.github.com>
2025-06-21 06:30:49 +0200

    completed backend extra challenge

--- backend/extra/intro-to-express/starter/src/routes/posts.js
+++ backend/extra/intro-to-express/starter/src/routes/posts.js
@@ -1,7 +1,21 @@
 import express from "express";
-import logger from "../middleware/logger.js";
-import { getPosts } from "../controllers/postsController.js";
+import { addTimestamps, updateTimestamp } from "../middleware/timesatamp.js";
+import { getPostById, createPost, updatePost, deletePost, handleGetPosts } from "../controllers/postsController.js";
+import { validatePost, validatePut } from "../middleware/validation.js";
 
 export const routes = express.Router();
 
-routes.get("/", logger, getPosts);
+// GET /posts or /posts?author=name
+routes.get("/", handleGetPosts); // j'ai corrigé ça 
+
+// GET /posts/:id
+routes.get("/:id", getPostById);
+
+// POST /posts to add a new blog post
+routes.post("/", validatePost, addTimestamps, createPost);
+
+// PUT /posts/:id
+routes.put("/:id", validatePut, updateTimestamp, updatePost);
+
+// DELETE /posts/:id
+routes.delete("/:id", deletePost);
