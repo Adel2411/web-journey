9f1aed4dc05604133342c3c614abde3270dd7012
yasmine bounasla <yasminebounasla@gmail.com>
2025-06-24 18:55:15 +0200

    Add showNotesForm state and pass props to Timer and NotesList

--- frontend/extra/intro-to-react/starter/src/App.jsx
+++ frontend/extra/intro-to-react/starter/src/App.jsx
@@ -2,14 +2,23 @@ import "./App.css";
 import Timer from "./components/Timer";
 import NotesList from "./components/NotesList";
 import Motivation from "./components/Motivation";
+import { useState } from "react";
 
 function App() {
+
+  const [showNotesForm, setShowNotesForm] = useState(false);
+
+
   return (
     <div className="app-container">
-      <h1 className="app-heading">Hello World!</h1>
 
-      <Timer />
-      <NotesList />
+      <Timer setShowNotesForm={setShowNotesForm} />
+
+      <NotesList 
+        showNotesForm={showNotesForm}
+        setShowNotesForm={setShowNotesForm}
+      />
+
       <Motivation />
     </div>
   );
