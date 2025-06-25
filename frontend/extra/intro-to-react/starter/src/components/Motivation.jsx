bc1aea35867ee34207fd0f29edbe7747b5c72cea
yasmine bounasla <yasminebounasla@gmail.com>
2025-06-25 20:55:27 +0200

    create Motivation component

--- frontend/extra/intro-to-react/starter/src/components/Motivation.jsx
+++ frontend/extra/intro-to-react/starter/src/components/Motivation.jsx
@@ -1,7 +1,26 @@
 import "../styles/Motivation.css";
+import { useState } from "react";
 
 function Motivation() {
-  return <div className="motivation">Motivation</div>;
+  const [like, setLike] = useState(0)
+
+  const handleLikes = () =>{
+    if(like < 5) setLike((prev) => prev+1)
+  }
+
+  return (
+    <div className="motivation">
+      <button
+        className={`motivation-btn level-${like}`}
+        onClick={handleLikes}
+        disabled={like === 5}
+      >
+        👍 Boost Motivation !
+      </button>
+
+      {like === 5 && <p className="motivation-msg">Nice focus. Keep it up! 🌟🚀</p>}
+    </div>
+  )
 }
 
 export default Motivation;
