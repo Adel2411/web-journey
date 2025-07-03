e88524ceac8fc927c5513a3f5462c4b2ed3e1fc6
Lina <linachihab836@gmail.com>
2025-07-03 14:19:54 +0100

    add the validation middelware

--- project/backend/routes/NoteRoute.js
+++ project/backend/routes/NoteRoute.js
@@ -2,8 +2,9 @@ import express from 'express';
 
 const router = express.Router();
 import { getNotes,postNotes } from '../controller/NoteController.js';
+import { createNoteValidator } from '../middelware/validation.js';
 
 router.get('/',getNotes);
-router.post('/',postNotes);
+router.post('/add',createNoteValidator,postNotes);
 
 export default router;
