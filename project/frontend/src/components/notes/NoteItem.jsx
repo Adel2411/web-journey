9ee8fa913030bae283e1bbec894bb083892375e7
MrmAya <kahlimeriemaya@gmail.com>
2025-07-04 01:35:35 +0200

    modification sur NoteItem + ajout de Note-Card pour afficher chaque note completement avec ces details

--- project/frontend/src/components/notes/NoteItem.jsx
+++ project/frontend/src/components/notes/NoteItem.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
-import './NoteItem.css'; 
+import './NoteItem.css';
 
-export default function NoteItem({ note }){
-    const date = new Date(note.createdAt).toLocaleDateString();
-
-    return(
-        <div className="note-item">
+export default function NoteItem({ note, onClick }) {
+  return (
+    <div className="note-item" onClick={onClick}>
           <h2 className="note-title" >{note.title}</h2>
           <p className="note-content">
             {note.content.length > 100 ? note.content.slice(0, 100) + "..." : note.content}
             </p>
             <div className="note-metadata">
-              <span>By {note.authorName} • {date}</span>
+              <span>By {note.authorName} • {new Date(note.createdAt).toISOString().split('T')[0]}</span>
             </div>
             <span className={`note-tag ${note.isPublic ? 'note-public' : 'note-private'}`}>
-            {note.isPublic ? "Public" : "Privé"}
+              {note.isPublic ? "Public" : "Privé"}
             </span>
         </div>
-    );
+  );
 }
--- project/frontend/src/components/notes/NoteList.jsx
+++ project/frontend/src/components/notes/NoteList.jsx
@@ -2,12 +2,22 @@ import React, { useState } from 'react';
 import { sampleNotes } from '../../data/sampleNotes';
 import NoteItem from './NoteItem';
 import './NoteList.css';
+import NoteCard from './NoteCard';
 
 export default function NoteList() {
   const [notes] = useState(sampleNotes);
   const [filter, setFilter] = useState('all');
+  const [selectedNote, setSelectedNote] = useState(null);
 
   const filteredNotes = filter === 'all' ? notes : notes.filter(n => n.authorName === "John Doe");
+
+  if (selectedNote) {
+    return (
+      <div className="note-card-container">
+        <NoteCard note={selectedNote} onClose={() => setSelectedNote(null)} />
+      </div>
+    );
+  }
 
   return (
     <div className="NoteList-container">
@@ -22,7 +32,9 @@ export default function NoteList() {
         <p className="text-no-note">No notes to display.</p>
       ) : (
         <div className="note-grid">
-          {filteredNotes.map(note => <NoteItem key={note.id} note={note} />)}
+          {filteredNotes.map(note => (
+            <NoteItem key={note.id} note={note} onClick={() => setSelectedNote(note)} />
+          ))}
         </div>
       )}
     </div>
