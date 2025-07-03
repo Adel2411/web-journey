3a514fa6a8f1daeab85e48712664d01552400d3d
MrmAya <kahlimeriemaya@gmail.com>
2025-07-04 00:05:50 +0200

    deplacer vers project

--- project/backend/src/app.js
+++ project/backend/src/app.js
@@ -1,14 +1,39 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import notesRouter from './routes/notes.js';
 
-const PORT = process.env.PORT || 3000;
+dotenv.config();
+
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({ 
+    origin: "http://localhost:5173", 
+    credentials: true 
+}));
 app.use(express.json());
 
-app.get("/", (_, res) => {
-  res.send("Hello, World!");
+app.get('/api/',(req,res)=>{
+    res.json({message: "Running !"});
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.use("/api/notes", notesRouter);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  if (err.code === "P1001") {
+    return res.status(503).json({ error: "Database connection error." });
+  }
+
+  if (err.name === "PrismaClientKnownRequestError") {
+    return res.status(400).json({ error: err.message });
+  }
+  res.status(500).json({ error: "Something went wrong!" });
 });
+
+app.listen(PORT, ()=>{
+    console.log(`Server is runnnig on ${PORT}`);
+})
