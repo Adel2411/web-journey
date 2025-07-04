1b9fddc259bb814f821c1b78a46ca04d066af8c9
Alaames <mesguialae@gmail.com>
2025-07-04 22:44:54 +0200

    Basic Note Management API done, only testing left

--- project/backend/src/app.js
+++ project/backend/src/app.js
@@ -1,14 +1,32 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import notesRouter from './routes/notes.js';
 
-const PORT = process.env.PORT || 3000;
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
+app.use("/api/notes", notesRouter);
+
 app.get("/", (_, res) => {
-  res.send("Hello, World!");
+  res.json({ message: "CollabNote API is running!" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Something went wrong!" });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`CollabNote API server is running on http://localhost:${PORT}`);
 });
