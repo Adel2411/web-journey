9934d4bd7070d6ad3639cbf7147aef96acf80ead
amiraber <155098066+amiraber@users.noreply.github.com>
2025-07-05 19:48:14 +0200

    PUT POST DELETE API

--- project/backend/src/controllers/notesController.js
+++ project/backend/src/controllers/notesController.js
@@ -26,3 +26,55 @@ export const getNoteById = async (req, res) => {
       res.status(500).json({ error: "Failed to fetch the note"}); 
     }
 };
+
+// PUT to update note by id
+export const updateNote = async (req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+        const { title, content, authorName, isPublic } = req.body;
+        const existe = await prisma.note.findUnique({ where: { id }});
+        if (!existe) {
+            return res.status(404).json({ error: "Note not found"});
+        }
+        const updatedNote = await prisma.note.update(
+            { where: { id }, data: { title, content, authorName, isPublic, updatedAt: new Date(),}}
+        );
+        res.json(updatedNote);
+    } catch (err) {
+       console.error(err);
+       res.status(500).json({ error: "Something went wrong!"});
+    }
+};
+
+// POST to create a note
+export const createNote = async (req, res) => {
+    try {
+        const { title, content, authorName, isPublic } = req.body;
+        if (!title || !content) {
+            return res.status(400).json({ error: "Title and content are required"});
+        }
+        const newNote = await prisma.note.create(
+            { data: { title, content, authorName: authorName || null, isPublic: isPublic !== undefined ? isPublic : true, createdAt: new Date(), updatedAt: new Date() }}
+        );
+        res.status(201).json(newNote);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Something went wrong!"});
+    }
+};
+
+// DELETE note by id
+export const deleteNote = async (req, res) => {
+   try {
+        const { id } = req.params;
+        const existing = await prisma.note.findUnique({ where: { id: parseInt(id) }});
+        if (!existing) {
+            return res.status(404).json({ error: "Note not found"});
+        }
+        await prisma.note.delete({ where: { id: parseInt(id) }});
+        res.json({ message: `Note ${id} deleted successfully` });
+   } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Something went wrong!" });
+    }
+};
--- project/backend/src/routes/notes.js
+++ project/backend/src/routes/notes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllNotes, getNoteById } from "../controllers/notesController.js";
+import { createNote, deleteNote, getAllNotes, getNoteById, updateNote } from "../controllers/notesController.js";
 
 const router = express.Router();
 
@@ -8,5 +8,14 @@ router.get("/", getAllNotes);
 
 // GET /api/notes/:id
 router.get("/:id", getNoteById);
+
+//PUT /api/notes/:id
+router.put("/:id", updateNote);
+
+//POST /api/notes
+router.post("/", createNote);
+
+//DELETE /api/notes/:id
+router.delete("/:id", deleteNote);
 
 export default router;
