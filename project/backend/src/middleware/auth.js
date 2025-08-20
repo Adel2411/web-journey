fba7fc8aaac29e1eac7c8cfa4ee3b8e06e81a43a
yoris29 <ydoumi29@gmail.com>
2025-08-20 11:02:43 +0100

    Added jwt auth middleware

---
+++ project/backend/src/middleware/auth.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv";
+dotenv.config()
+
+const authToken = (req, res, next) => {
+	const authHeader = req.headers["authorization"]);
+	if (authHeader) {
+		const token = authHeader.split(" ")[1];
+		const isMatch = jwt.verify(token, process.env.JWT_SECRET, (err, user) {
+			if (err) {
+				return res.status(401).json({ err: true, msg: "Invalid token" });
+			}
+			req.user = user;
+		})
+	}
+	next();
+};
+
+export default authToken;
