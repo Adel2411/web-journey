637c33866cc570ff125f1d09940bb2997db47fb5
yasmine bounasla <yasminebounasla@gmail.com>
2025-08-25 19:16:11 +0200

    etup AuthContext with login, register and logout functions

---
+++ project/frontend/src/contexts/AuthContext.jsx
@@ -0,0 +1,67 @@
+import { createContext, useState, useEffect } from "react";
+import { loginUser, registerUser } from "../services/api";
+
+
+export const AuthContext = createContext()
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true)
+
+
+
+    useEffect(()=> {
+        const token = localStorage.getItem("token")
+
+        if(token){
+            setUser({name : "test"})
+        }
+
+        setLoading(false)
+    }, [])
+
+    //login function
+
+    const login = async (email, password) => {
+        try {
+            const response = await loginUser(email, password);
+            localStorage.setItem("token", response.token);
+
+            setUser(response.user)
+            return { success : true }
+
+        } catch(err) {
+            return { success: false, message: err.message };
+        }
+    }
+
+    //register function
+
+    const register = async (userData) => {
+        try{
+            const response = await registerUser(userData)
+            localStorage.setItem("token", response.token)
+
+            setUser(response.user);
+            return { success : true }
+
+        } catch (err) {
+            return { success: false, message: err.message };
+        }
+    }
+
+    //logout function
+
+    const logout = () => {
+        localStorage.removeItem("token");
+        setUser(null);
+    }
+
+
+    return(
+        <AuthContext.Provider value={{ user, login, register, logout, loading }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
