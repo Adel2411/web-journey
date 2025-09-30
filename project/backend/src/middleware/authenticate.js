8db551bb9882d43ca559f2916d9fdab9ba268163
Alaames <mesguialae@gmail.com>
2025-09-30 02:01:41 +0200

    added the authentication middleware

--- project/backend/src/middleware/authenticate.js
+++ project/backend/src/middleware/authenticate.js
@@ -1,3 +1,24 @@
+import { verifyJWT } from "../utils/jwt.js";
+
 export function authenticate(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).send({ message: "Authorization header is missing or invalid" });
+    }
+
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+        return res.status(401).send({ message: "Token is missing" });
+    }
+    try {
+        const decoded = verifyJWT(token);
+        if (!decoded) {
+            return res.status(401).send({ message: "Invalid token" });
+        }
+        req.user = decoded;
+    } catch (error) {
+        return res.status(401).send({ message: "Not Authorized" });
+    }
+    next();
 
 }
